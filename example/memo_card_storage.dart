04d04a27411a7d8461533713a601571a5d7a0032
flatFeather <github.astronaut143@silomails.com>
2024-07-08 19:21:56 +0200

    Add store example

---
+++ example/memo_card_storage.dart
@@ -0,0 +1,29 @@
+import 'package:hive/hive.dart';
+import 'package:memo_assistant/src/infrastructure/models/memo_card_dao.dart';
+
+void main() async {
+  // Initialize Hive
+  Hive.init('hive_test_directory');
+  
+  // Register the generated adapter
+  Hive.registerAdapter(MemoCardDAOAdapter());
+
+  // Open the box
+  var box = await Hive.openBox<MemoCardDAO>('memoCardBox');
+
+  // Create an instance of MemoCardDAO
+  var memoCard = MemoCardDAO('Knowledge Example', DateTime.now().add(Duration(days: 1)));
+
+  // Store the instance in the box
+  await box.put('exampleKey', memoCard);
+
+  // Retrieve the instance from the box
+  var storedMemoCard = box.get('exampleKey');
+
+  print('Stored nodeHash: ${storedMemoCard?.nodeHash}');
+  print('Stored due date: ${storedMemoCard?.due}');
+
+  // Delete the box after the test if desired
+  await box.deleteFromDisk();
+  print('Box deleted');
+}
