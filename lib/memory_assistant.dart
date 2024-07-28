6211e8341a0a93a2ddac9773ca2643572b89e599
flatFeather <github.astronaut143@silomails.com>
2024-07-29 00:10:34 +0200

    Refactor memory assistant description to match README description

--- lib/memory_assistant.dart
+++ lib/memory_assistant.dart
@@ -1,8 +1,9 @@
 // TODO: Complete the copyright.
 // Copyright (c) 2024, ...
 
-/// Module designed for critical memorization tasks using principles from Anki and SuperMemo. 
-/// This module focuses on two distinct types of memorization
+/// This repository contains the logic of the memorization assistant logic implementation, 
+/// thus, we can modify this implementation without affecting any dependent components, adhering to principles of modularity and single responsibility. 
+/// The graphic user interface that use this module is in t3-Vault repository.
 ///
 // TODO: Complete the library-level documentation comments.
 library;
