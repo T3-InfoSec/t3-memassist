89c1cba9d4ac8f857685206a147107e92ac1174d
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-08-24 12:04:02 +0300

    Fix doc comments.

--- lib/memory_assistant.dart
+++ lib/memory_assistant.dart
@@ -1,12 +1,8 @@
 // TODO: Complete the copyright.
 // Copyright (c) 2024, ...
 
-/// This repository employs the Spaced Repetition Scheduler algorithm to assist users in memorising cards.
-///
-/// This contains the logic of the memorisation assistant, following the principles of modularity and single responsibility,
-/// so it can be modified without affecting any dependent components.
-/// The graphic user interface that uses this module is in the t3-Vault repository.
+/// This repository employs the Spaced Repetition Scheduler algorithm
+/// to assist users in memorizing knowledge.
 library;
 
 export 'src/memo_card.dart';
-// TODO: Export any libraries intended for clients of this package.
--- lib/src/memo_card.dart
+++ lib/src/memo_card.dart
@@ -1,12 +1,12 @@
 import 'package:fsrs/fsrs.dart';
 
-/// A class that represents a memorization card containing knowledge
+/// A class that represents a memorization card containing knowledge.
 ///
-/// It uses the spaced repetition algorithm [FSRS]: https://pub.dev/packages/fsrs
-/// and [_card] to manage memorization card revision from rate.
-/// [_log] stores details of a card review that are essential for the spaced repetition
-/// algorithm to optimize future review schedules. It allows us to to keep a record of each review session,
-/// which can be useful for analyzing learning progress and patterns over time.
+/// It uses the spaced repetition algorithm [FSRS ref]: https://pub.dev/packages/fsrs
+/// to manage memorization card. It stores details of a card review that are
+/// essential for the spaced repetition algorithm to optimize future review
+/// schedules. It allows to to keep a record of each review session, which can
+/// be useful for analyzing learning progress and patterns over time.
 class MemoCard {
   final Map<String, dynamic>? _knowledge;
   final FSRS _algorithm = FSRS();
@@ -19,7 +19,8 @@ class MemoCard {
 
   /// Rates the card knowledge and updates its state and review log.
   ///
-  /// The [rating] parameter can take the values 'again', 'hard', 'good', and 'easy'.
+  /// The [rating] parameter can take the values 'again', 'hard', 'good',
+  /// and 'easy'.
   void rateCard(String rating) {
     DateTime now = DateTime.now().toUtc();
     Map<String, Rating> rates = {
