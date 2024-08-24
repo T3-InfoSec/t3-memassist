a67abee3e25f1c39637c4685dc78b6ab6198d479
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-08-24 12:24:13 +0300

    Enhance doc comments.

--- lib/src/memo_card.dart
+++ lib/src/memo_card.dart
@@ -2,7 +2,7 @@ import 'package:fsrs/fsrs.dart';
 
 /// A class that represents a memorization card containing knowledge.
 ///
-/// It uses the spaced repetition algorithm [FSRS ref]: https://pub.dev/packages/fsrs
+/// It uses the spaced repetition algorithm [FSRS ref](https://pub.dev/packages/fsrs)
 /// to manage memorization card. It stores details of a card review that are
 /// essential for the spaced repetition algorithm to optimize future review
 /// schedules. It allows to to keep a record of each review session, which can
@@ -14,7 +14,7 @@ class MemoCard {
   ReviewLog? _log;
 
   /// Constructor that initializes a MemoCard with optional [knowledge].
-  /// If no knowledge is provided, [_knowledge] will be null.
+  /// If no knowledge is provided, the default value is null.
   MemoCard([this._knowledge]);
 
   /// Rates the card knowledge and updates its state and review log.
@@ -35,8 +35,7 @@ class MemoCard {
     _log = schedulingCards[rates[rating]]!.reviewLog;
   }
 
-  /// Getter for [_knowledge].
-  /// Returns the knowledge or an empty map if _knowledge is null.
+  /// Returns the knowledge or an empty map if there is no knowledge.
   Map<String, dynamic>? get knowledge => _knowledge;
 
   /// [due] represents the date and time of the next card revision.
